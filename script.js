3103115f957ff8a325fe54c3dd3e4a62efe6fc3a
Vinaya-16 <vinayapatole16@gmail.com>
2025-08-15 19:26:25 +0530

    Succesfully done

--- script.js
+++ script.js
@@ -1,19 +1,67 @@
 
-const input = document.getElementById("input-mood");
+const k1 = document.getElementById("kishor-kumar");
+const trend = document.getElementById("trending");
+const musical = document.getElementById("musical");
+const beats = document.getElementById("good-beats");
+const happy = document.getElementById("happy");
+
 const btn = document.getElementById("play-btn");
 
-btn.addEventListener("click", function () {
-    const mood = input.value;
-    uri = encodeURIComponent(mood);
+btn.addEventListener("click",()=>{
+  
+    if(k1.checked){
+        document.body.innerHTML = `
+        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/43ehHE4nhmYH976HupqUlx?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        `
+    }
 
-    // const search = `https://open.spotify.com/episode/7makk4oTQel546B0PZlDM5`
-    const search = `https://open.spotify.com/embed/search/${uri}`;
+    else if(trend.checked){
+        document.body.innerHTML = `
+        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX16doZpno3Aw?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        `
+    }
 
-    const result = document.getElementById("result");
+    else if(musical.checked){
+        document.body.innerHTML = `
+        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DXcCnTAt8CfNe?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        `
+    }
 
-    document.body.innerHTML = `
-    <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/43ehHE4nhmYH976HupqUlx?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    `
+    else if(beats.checked){
+        document.body.innerHTML = `
+        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DXcNb6Ba0LuVc?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        `
+    }
+
+    else if(happy.checked){
+        document.body.innerHTML = `
+        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DXdPec7aLTmlC?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        `
+    }
 
 })
+
+
+// kishore kumar
+//     <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/43ehHE4nhmYH976HupqUlx?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+
+//trending vibes
+// <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX16doZpno3Aw?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+
+// //Musical Theraphy
+// <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DXcCnTAt8CfNe?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+
+// //Feel good beats
+// <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DXcNb6Ba0LuVc?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+// //Happy hits
+// <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DXdPec7aLTmlC?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+
+
+
+
 
